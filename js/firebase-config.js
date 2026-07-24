eea6767ff4c47ab51ab824fd20751ffd4f2d02fa
crystalalznq <crystalalznq@gmail.com>
2026-07-24 23:06:16 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyASsdEASM5NEUZ7X71glbfMCVYw44WJUJI",
+  authDomain: "crystalalznq-d6f7d.firebaseapp.com",
+  projectId: "crystalalznq-d6f7d",
+  storageBucket: "crystalalznq-d6f7d.firebasestorage.app",
+  messagingSenderId: "825883317223",
+  appId: "1:825883317223:web:18caa48d7dfb8c0664e342",
+  measurementId: "G-SQBVZ03071"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
